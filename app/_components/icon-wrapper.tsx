a47e869b1d0453f2afb1b1da9b96c2e6c74c1956
Karan Hathwar <karanhathwar@gmail.com>
2024-11-27 22:16:19 +0530

    dashboard updates

--- app/_components/icon-wrapper.tsx
+++ app/_components/icon-wrapper.tsx
@@ -1,7 +1,8 @@
-export default function Icon({ icon, size = '24px' }: {icon: string; size?: string;}) {
+export default function Icon({ icon, size = '24px', fill=0 }: {icon: string; size?: string; fill?: number;}) {
     return (     
-        <span className="material-symbols-rounded" style={{ fontSize: size }}>
-            {icon}
+        // <span className={`material-symbols-rounded ${fill? 'filled' : ''}`} style={{ fontSize: size }}>
+        <span className={`material-symbols-rounded`} style={{ fontSize: size, fontVariationSettings: `'FILL' ${fill}, 'wght' 400, 'GRAD' 0, 'opsz' 24`} }>
+        {icon}
         </span>
     )
 }
--- app/host/dashboard/page.tsx
+++ app/host/dashboard/page.tsx
@@ -57,14 +57,14 @@ function DashboardNav() {
 
 function DashboardSidebar({ tab }: { tab: string }) {
   return (
-    <section className="bg-eventr-gray flex flex-col gap-1.5 w-48 p-4 border-r-2 border-zinc-700">
+    <section className="bg-eventr-gray flex flex-col gap-2.5 w-64 flex-shrink-0 p-4 border-r-2 min-h-screen border-zinc-700">
       <Link
         className={`flex items-center px-2 py-1 gap-2 rounded-md duration-200 ${
           tab === "dashboard" && "bg-eventr-main font-bold py-3"
         }`}
         href={"?tab=dashboard"}
       >
-        <Icon icon="grid_view" />
+        <Icon icon="grid_view" fill={tab === "dashboard" ? 1 : 0}/>
         <p>Dashboard</p>
       </Link>
       <Link
@@ -73,7 +73,7 @@ function DashboardSidebar({ tab }: { tab: string }) {
         }`}
         href={"?tab=events"}
       >
-        <Icon icon="analytics" />
+        <Icon icon="analytics" fill={tab === "events" ? 1 : 0}/>
         <p>Events</p>
       </Link>
       <Link
@@ -82,7 +82,7 @@ function DashboardSidebar({ tab }: { tab: string }) {
         }`}
         href={"?tab=analytics"}
       >
-        <Icon icon="event_note" />
+        <Icon icon="event_note" fill={tab === "analytics" ? 1 : 0}/>
         <p>Analytics</p>
       </Link>
       <Link
@@ -91,7 +91,7 @@ function DashboardSidebar({ tab }: { tab: string }) {
         }`}
         href={"?tab=payments"}
       >
-        <Icon icon="payments" />
+        <Icon icon="payments" fill={tab === "payments" ? 1 : 0}/>
         <p>Payments</p>
       </Link>
       <Link
@@ -100,7 +100,7 @@ function DashboardSidebar({ tab }: { tab: string }) {
         }`}
         href={"?tab=promoters"}
       >
-        <Icon icon="person_play" />
+        <Icon icon="person_play" fill={tab === "promoters" ? 1 : 0}/>
         <p>Promoters</p>
       </Link>
       <Link
@@ -109,7 +109,7 @@ function DashboardSidebar({ tab }: { tab: string }) {
         }`}
         href={"?tab=customers"}
       >
-        <Icon icon="chat" />
+        <Icon icon="chat" fill={tab === "customers" ? 1 : 0}/>
         <p>Messages</p>
       </Link>
     </section>
@@ -305,8 +305,8 @@ function EventCard() {
   return (
     <div className="relative flex bg-eventr-gray w-full h-[90px] rounded-md">
       <div className="relative w-40 h-full rounded-l-md bg-zinc-600"></div>
-      <div className="flex gap-3 items-center">
-        <div className="flex-grow p-2 w-96">
+      <div className="w-full flex gap-3 items-center">
+        <div className="flex-grow p-2">
           <h3 className="font-bold text-lg">
             Event Name Really long and informative
           </h3>
@@ -316,25 +316,99 @@ function EventCard() {
         <p className="w-28">Nov 11, 2024</p>
         <p className="w-20">NA</p>
         <p className="w-24">24,500Rs</p>
-        <div className="flex items-center text-amber-500 w-16">
-          <p className="text-white">4.5</p>
-          <Icon icon="star" />
-        </div>
-        <Link href={`?tab=events&view=${'id'}`} className="flex items-center"><Icon icon='settings'/></Link>
+
+        <Link href={`?tab=events&view=${'id'}`} className="flex items-center w-12"><Icon icon='settings'/></Link>
       </div>
     </div>
   );
 }
 
-function Events({ view }: { view: string | null }) {
+function ViewEvent(){
   return (
-    <div className="flex p-4 gap-4 w-full">
-      {view ? (
-      <h1>
-        <div className="w-full aspect-h-9 aspect-w-16 bg-zinc-500 rounded-md">
+  <div className="relative w-full flex flex-col gap-2 bg-eventr-gray rounded-md">
+    <div className="relative w-full aspect-w-7 aspect-h-3 bg-zinc-600 rounded-md">
+    </div>
+    <div className="p-4 flex flex-col">
+      <div className="flex w-full gap-2.5">
+        <div className="flex flex-col flex-grow">
+          <h2 className="text-2xl font-bold">Event Name Really long and informative</h2>
+          <div className="flex items-center gap-1"><Icon icon='location_on' size="18px"/> Blah blah square, Blah City</div>
         </div>
-      </h1>
-      ) : (
+        <Link href={`?tab=events&edit=${'id'}`} className="flex items-center gap-1 font-bold text-lg px-4 h-10 rounded-md border border-zinc-700"><Icon icon="edit" size="18px"/> Edit Details</Link>
+        <Link href={`?tab=events&manage=${'id'}`} className="flex items-center gap-1 font-bold text-lg px-4 h-10 rounded-md border border-zinc-700"><Icon icon="settings" size="18px"/> Manage</Link>
+        <Link href={`?tab=analytics&event=${'id'}`} className="flex items-center gap-1 font-bold text-lg px-4 h-10 rounded-md bg-eventr-main"><Icon icon="analytics" size="18px"/> View Analytics</Link>
+      </div>
+      <div className="flex items-center gap-2.5 text-sm mt-2">
+        <p className="px-2 py-0.5 rounded-md bg-amber-600">Comedy</p>
+        <p className="px-2 py-0.5 rounded-md bg-eventr-main">18+</p>
+        <p className="px-2 py-0.5 rounded-md border text-zinc-300 border-zinc-700">Tag 1</p>
+        <p className="px-2 py-0.5 rounded-md border text-zinc-300 border-zinc-700">Tag 2</p>
+        <p className="px-2 py-0.5 rounded-md border text-zinc-300 border-zinc-700">Tag 3</p>
+      </div>
+
+      <div className="w-full h-[1px] bg-zinc-700 my-3"/>
+      <div className="flex gap-4">
+        
+        <div className="flex flex-col gap-3">
+          <h2 className="font-bold text-lg">About:</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget
+          justo nec justo facilisis fermentum. Curabitur et justo eget justo
+          facilisis fermentum. Curabitur et justo eget justo facilisis
+          fermentum. Curabitur et justo eget justo facilisis fermentum.
+          </p>
+          <h2 className="font-bold text-lg">Instructions:</h2>
+          <ul className="list-disc translate-x-4">
+            <li>Wear a mask</li>
+            <li>Carry your ID</li>
+            <li>Carry your ticket</li>
+          </ul>
+          <h2 className="font-bold text-lg">Performers:</h2>
+          <div className="border border-zinc-600 min-w-36 w-fit h-48 rounded-md">
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <ul className="flex flex-col gap-2 h-fit bg-zinc-600 p-4 rounded-md w-80 flex-shrink-0">
+            <li className="flex items-center gap-2"><Icon icon='confirmation_number' size="18px"/>Tickets from ₹300</li>
+            <li className="flex items-center gap-2"><Icon icon='event' size="18px"/>21st Dec, 9:00 AM onwards</li>
+            <li className="flex items-center gap-2"><Icon icon='schedule' size="18px"/>4 Hours</li>
+            <li className="flex items-center gap-2"><Icon icon='stadium' size="18px"/>1200 attendees</li>
+          </ul>
+          <div className="bg-zinc-700 h-[1px] mt-1"/>
+          <div className="w-80">
+            <h2 className="text-lg font-bold mb-2">
+              About the host:
+            </h2>
+            <div className="flex gap-2">
+              <div className="h-24 w-24 flex-shrink-0 bg-zinc-600 rounded-full"></div>
+              <div>
+                <h3>Host Name</h3>
+                <h4 className="text-zinc-300 text-sm -translate-y-1">Company Name</h4>
+                <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>  
+    </div>
+  </div>
+  )
+}
+
+function EditEvent(){
+  return (
+    <div>
+      Hi
+    </div>
+  )
+}
+
+function Events({ view, edit, manage }: { view: string | null, edit: string | null, manage: string | null }) {
+  return (
+    <div className="flex p-4 gap-4 w-full h-full relative">
+      {view && <ViewEvent />}
+      {edit && <EditEvent />}
+      {!view && !edit &&
       <section id="events-list" className="flex flex-grow flex-col gap-4">
         <div id="latest-event" className="flex flex-col gap-1.5">
           <h2 className="font-bold text-xl mb-1">Lastest Event</h2>
@@ -343,7 +417,6 @@ function Events({ view }: { view: string | null }) {
             <div className="flex-grow"></div>
           </div>
         </div>
-
         <div id="all events" className="flex flex-col gap-1.5">
           <div className="flex gap-1.5 mb-1">
             <h2 className="text-xl font-bold flex-grow w-full">All Events</h2>
@@ -358,41 +431,24 @@ function Events({ view }: { view: string | null }) {
             </div>
           </div>
           <div className="flex gap-3 text-zinc-400">
-            <p className="w-[544px]">Event</p>
+            <p className="flex-grow">Event</p>
             <p className="w-24">Status</p>
             <p className="w-28">Start Date</p>
             <p className="w-20">Ticketing</p>
             <p className="w-24">Revenue</p>
-            <p className="w-16">Rating</p>
+            <p className="w-12"> </p>
           </div>
           <EventCard />
         </div>
       </section>
-      /* <section id="artists-list">
-        <div className="bg-eventr-gray p-2 rounded-md">
-          <h2 className="text-center text-xl font-bold">Artists</h2>
-          <div className="flex flex-col"></div>
-        </div>
-        <div className="mt-4">
-          <h2 className="text-center text-xl font-bold mb-2">What's New</h2>
-          <div className="flex flex-col justify-center gap-4 bg-gradient-to-br from-cyan-600 to-slate-600 py-6 p-4 rounded-md">
-            <h3 className="text-2xl font-bold leading-none text-center">
-              Take A Quick Tour
-            </h3>
-            <button className="bg-white text-black px-2 rounded-md font-bold text-xl">
-              Start
-            </button>
-          </div>
-        </div>
-      </section> */
-      )}
+      }
     </div>
   );
 }
 
 function Analytics() {
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full relative h-screen">
       <section id="anayltics-nav" className="flex gap-2 p-4">
         <div className="relative flex-grow">
           <select className="bg-eventr-gray p-1.5 w-full rounded-md appearance-none outline-none">
@@ -444,18 +500,19 @@ export default function Page() {
   const router = useRouter();
   const tab = params.get("tab");
   const view = params.get("view");
+  const edit = params.get("edit");
+  const manage = params.get("manage");
   if (!tab) {
     router.replace("/host/dashboard?tab=dashboard");
   }
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col relative">
       <DashboardNav />
-
       <div className="flex flex-grow">
         <DashboardSidebar tab={tab || "dashboard"} />
         <div className="flex-grow">
           {tab === "dashboard" && <Dashboard />}
-          {tab === "events" && <Events view={view}/>}
+          {tab === "events" && <Events view={view} edit={edit} manage={manage}/>}
           {tab === "analytics" && <Analytics />}
           {tab === "payments" && <h1>Payments</h1>}
           {tab === "promoters" && <h1>Promoters</h1>}
